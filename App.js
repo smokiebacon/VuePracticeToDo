fd3ecfb1f7985b8069eac08bbe623330289c0898
Abraham Hsu <smokiebacon@gmail.com>
2019-06-27 19:26:52 -0700

    print todos when submitting with v-for

--- App.js
+++ App.js
@@ -2,6 +2,16 @@ const app = new Vue({
     //create element of id: 'app' where our app will live.
     el: '#app',
     data: {
-        title: 'Hello Coder!'
+        title: 'Hello Coder!',
+        newToDo: '',  /* links to v-model in <input> instead of using onChange in React*/
+        toDos: []
+    },
+    methods: {
+        addToDo() {
+            this.toDos.push({
+                title: this.newToDo,
+                done: false
+            })
+        }
     }
 })
